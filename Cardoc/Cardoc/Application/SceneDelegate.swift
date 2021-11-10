734a6c65829018eb0969f4923b9a6969f6d73e9d
shimyoungmin <dudals6965@gmail.com>
2021-11-10 21:51:40 +0900

    feat: modify SceneDelegate 코디네이터 패턴을 이용하여 window의 rootviewController 지정해줌

--- Cardoc/Cardoc/Application/SceneDelegate.swift
+++ Cardoc/Cardoc/Application/SceneDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private let appDIContainer = AppDiContainer()
+    private var flowCoordinator: AppFlowCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let searchingUserViewController = appDIContainer.makeSearchingUserViewController()
+        let rootViewController = UINavigationController(rootViewController: searchingUserViewController)
+        flowCoordinator = AppFlowCoordinator(with: rootViewController, with: appDIContainer)
+        searchingUserViewController.injectionCoordinator(with: flowCoordinator!)
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
     }
 }
 
