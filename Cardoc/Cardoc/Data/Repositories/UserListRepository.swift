5663e9e289bc614373a22fd45220c12c07d1e433
shimyoungmin <dudals6965@gmail.com>
2021-11-10 16:13:43 +0900

    feat: Add UserListRepository

--- Cardoc/Cardoc/Data/Repositories/UserListRepository.swift
+++ Cardoc/Cardoc/Data/Repositories/UserListRepository.swift
@@ -10,15 +10,20 @@ import Alamofire
 import RxSwift
 
 protocol UserListRepositoryProtocol {
-    func fetch() -> Observable<UserList>
+    func fetch(with query: [String:Any]?) -> Observable<UserList>
 }
 
 class UserListRepository: UserListRepositoryProtocol {
     
-    private let networkTask: Task<Request
-    func fetch() -> Observable<UserList> {
-        <#code#>
+    private let networkTask: Task<SearchingRequest, UserListDTO>
+    
+    init(with networkTask: Task<SearchingRequest, UserListDTO>) {
+        self.networkTask = networkTask
     }
     
-    
+    func fetch(with query: [String:Any]? = nil) -> Observable<UserList> {
+        return networkTask.fetch(SearchingRequest(path: EndPoint.mainURL.description,
+                                                  httpMethod: .get, bodyParams: query, headers: nil), UserListDTO.self)
+            .map{$0.toDomain()}
+    }
 }
