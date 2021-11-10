aaab7776cb512e6ddd0d5e21f246f7a75469168a
shimyoungmin <dudals6965@gmail.com>
2021-11-10 21:17:06 +0900

    feat: Add AppDIContainer
    앱에서 각 Scene마다 의존성을 주입해주는 AppDIContainer 구현

--- Cardoc/Cardoc/Presentation/DIContainer/AppDIContainer.swift
+++ Cardoc/Cardoc/Presentation/DIContainer/AppDIContainer.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Alamofire
+
+class AppDiContainer: AppFlowCoordinatorDependencies {
+    var apiNetworkService: NetworkTask<SearchingRequest, UserListDTO> = NetworkTask(with: SearchingDispatcher(with: AF), with: JSONDecoder(), with: .convertFromSnakeCase)
+    
+    struct Dependencies {
+        let searchingUserTask: Task<SearchingRequest, UserListDTO>
+    }
+    
+    private let dependencies: Dependencies
+    
+    init(with dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+    
+    func makeUserListRepository() -> UserListRepositoryProtocol {
+        return UserListRepository(with: dependencies.searchingUserTask)
+    }
+    
+    func makeUserListUseCase() -> FetchUserListUseCaseProtocol {
+        return FetchUserListUseCase(with: makeUserListRepository())
+    }
+    
+    func makeUserListViewModel() -> UserListViewModel {
+        return UserListViewModel(with: makeUserListUseCase())
+    }
+    
+    func makeSearchingUserViewController() -> SearchingUserViewController {
+        return SearchingUserViewController.create(with: makeUserListViewModel())
+    }
+    
+    func makeAppFlowCoordinator(with navigationController: UINavigationController) -> AppFlowCoordinator {
+        return AppFlowCoordinator(with: navigationController, with: self)
+    }
+}
