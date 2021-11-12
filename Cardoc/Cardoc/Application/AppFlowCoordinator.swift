ba797b708457212564886c5d267a99e48bf30955
shimyoungmin <dudals6965@gmail.com>
2021-11-12 13:41:11 +0900

    modify: AppFlowCoordinator 유저목록화면에서 유저상세화면으로 넘어가는 코드 구현

--- Cardoc/Cardoc/Application/AppFlowCoordinator.swift
+++ Cardoc/Cardoc/Application/AppFlowCoordinator.swift
@@ -10,6 +10,7 @@ import UIKit
 
 protocol AppFlowCoordinatorDependencies {
     func makeSearchingUserViewController() -> SearchingUserViewController
+    func makeDetailUserViewController(with endPoint: String) -> DetailUserViewController
 }
 
 protocol Coordinator {
@@ -23,5 +24,11 @@ class AppFlowCoordinator: Coordinator {
     init(with rootViewController: UINavigationController, with dependencies: AppFlowCoordinatorDependencies) {
         self.rootViewController = rootViewController
         self.dependencies = dependencies
+    }
+    
+    func presentDetailUserViewController(with endPoint: String) {
+        let detailUserViewController = dependencies.makeDetailUserViewController(with: endPoint)
+        detailUserViewController.injectionCoordinator(with: self)
+        self.rootViewController.present(detailUserViewController, animated: true, completion: nil)
     }
 }
