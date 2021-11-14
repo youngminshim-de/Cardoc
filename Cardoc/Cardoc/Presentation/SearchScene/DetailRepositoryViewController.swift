15d0046f416c8610ab775e2be71aa1223b0788a9
shimyoungmin <dudals6965@gmail.com>
2021-11-14 13:58:41 +0900

    refactor: modify DetailRepositoryViewController

--- Cardoc/Cardoc/Presentation/SearchScene/DetailRepositoryViewController.swift
+++ Cardoc/Cardoc/Presentation/SearchScene/DetailRepositoryViewController.swift
@@ -13,6 +13,7 @@ class DetailRepositoryViewController: UIViewController, CustomHeaderViewDelegate
 
     @IBOutlet weak var headerView: DetailRepositoryHeaderView!
     @IBOutlet weak var readmeView: UIView!
+    @IBOutlet weak var readmeHeightConstraint: NSLayoutConstraint!
     private var markdownView: MarkdownView = MarkdownView()
     
     weak var coordinator: AppFlowCoordinator?
@@ -48,11 +49,16 @@ class DetailRepositoryViewController: UIViewController, CustomHeaderViewDelegate
     
     private func setupMarkDownView() {
         self.readmeView.addSubview(markdownView)
+        markdownView.isScrollEnabled = false
         markdownView.translatesAutoresizingMaskIntoConstraints = false
         markdownView.leadingAnchor.constraint(equalTo: readmeView.leadingAnchor).isActive = true
         markdownView.topAnchor.constraint(equalTo: readmeView.topAnchor).isActive = true
         markdownView.trailingAnchor.constraint(equalTo: readmeView.trailingAnchor).isActive = true
         markdownView.bottomAnchor.constraint(equalTo: readmeView.bottomAnchor).isActive = true
+
+        markdownView.onRendered = { [weak self] height in
+            self?.readmeHeightConstraint.constant = height
+        }
     }
     
     func bindingViewModel() {
